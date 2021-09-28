18cc77c340811f72cfcffc62e1e00b2fff347bd2
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-28 23:33:31 +0530

    ADV_C115_FilterWebAPP
    Added code for showing camera, training the poseNet model

--- main.js
+++ main.js
@@ -5,12 +5,31 @@ function preload() {
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoded);
+    poseNet.on('pose', gotResults);
 }
 
 function draw() {
-    //no code today
+    image(video, 0, 0, 400, 400);
 }
 
 function take_snapshot() {
     save('filterImage.png');
+}
+
+function modelLoded() {
+    console.log("PoseNet model is loaded");
+}
+
+function gotResults(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("Lip X : " + results[0].pose.lip.x);
+        console.log("Lip Y : " + results[0].pose.lip.y);
+    }
 }
