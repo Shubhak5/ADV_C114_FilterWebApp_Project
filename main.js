1ba7094bcf736e8a6a315908a7c40045034616e5
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-28 23:01:42 +0530

    ADV_C114_FilterWebAPP_Project
    Added basic html, css and js code

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    //no items to load
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw() {
+    //no code today
+}
+
+function take_snapshot() {
+    save('filterImage.png');
+}
