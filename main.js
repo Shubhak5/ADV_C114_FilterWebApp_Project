7056f1906dc7e00bb9e2123c40e06bed79339570
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-29 17:48:25 +0530

    ADV_C116_FilterWebApp
    Added JS code to add Mustache or Lipstick

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
-    //no items to load
+    clown_image = loadImage('https://i.postimg.cc/MKDH61sf/clownimage.png');
 }
 
 function setup() {
@@ -10,26 +13,37 @@ function setup() {
     video.size(400, 400);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoded);
-    poseNet.on('pose', gotResults);
+    poseNet = ml5.poseNet(video, "modelLoaded");
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 400, 400);
+
+    //draw red circle on the nose
+    fill(255, 0, 0); //fill red color
+    stroke(255, 0, 0); //border color=red
+    circle(noseX, noseY, 20);
+
+    //draw clown images
+    image(clown_image, noseX, noseY, 40, 40);
 }
 
 function take_snapshot() {
-    save('filterImage.png');
+    save('myFilterImage.png');
 }
 
-function modelLoded() {
-    console.log("PoseNet model is loaded");
+function modelLoaded() {
+    console.log("PoseNet Model is Loaded");
 }
 
-function gotResults(results) {
+function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("Lip X : " + results[0].pose.lip.x);
-        console.log("Lip Y : " + results[0].pose.lip.y);
+
+        noseX = results[0].pose.nose.x - 10;
+        noseY = results[0].pose.nose.y - 10;
+        console.log("Nose x: " + noseX);
+        console.log("Nose y: " + noseY);
     }
 }
